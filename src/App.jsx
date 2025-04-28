fe1f568bcc999f64c3b911b6a83916efed86e817
MirnaBuntic <mirnacb@hiof.no>
2025-04-28 22:24:57 +0200

    App.jsx, Layout.jsx, Main.jsx
    Satt upp routing i app.jsx.
    Skapat en layout struktur i layout.jsx.
    Fixat browserrouter i main.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import CategoryPage from "./components/CategoryPage";
+import EventPage from "./components/EventPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <Layout>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/event/:id" element={<EventPage />}></Route>
+        <Route path="/category/:slug" element={<CategoryPage />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+   </Layout>
   )
 }
 
