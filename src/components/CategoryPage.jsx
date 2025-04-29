d5fc40a92fc03128e6b5f7740972a032b2b53a6a
Danielladominey <danielhd@hiof.no>
2025-04-29 21:09:44 +0200

    Made categoryPage with Ekrav

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -3,6 +3,15 @@ import { useParams } from "react-router-dom";
 import categories from "./DataCategory";
 
 export default function CategoryPage () {
-  const { slug } = useParams
+  const { slug } = useParams ();
+
+  const category = categories.find((category) => category.slug === slug);
+  const categoryName = category ? category.name : "Ukjent kategori";
+
+  return (
+    <div>
+      <h1>{categoryName}</h1>
+    </div>
+  );
 }
 
