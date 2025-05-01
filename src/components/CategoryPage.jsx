c4447b1337d32bec8b08b4b1f5a2d9a22f5e004a
Danielladominey <danielhd@hiof.no>
2025-05-01 18:35:54 +0200

    Laget categorycard og categorypage
    Laget et planlagt gjenbrukbart categorycard og brukt det i categorypage. Funksjon for filtrering fungerer. Lagt til festival som keyword for musikk slik at jeg får opp festivaler.

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,17 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import CategoryCard from "./CategoryCard";
 import categories from "./DataCategory";
 
 export default function CategoryPage () {
   const { slug } = useParams ();
 
-  const category = categories.find((category) => category.slug === slug);
-  const categoryName = category ? category.name : "Ukjent kategori";
+  
+  const [events, setEvents] =useState([]);
+  const [attractions, setAttractions] = useState([]);
+  const [filters, setFilters] = useState({
+    keyword: "",
+    city: "",
+    country: "",
+    date: "",
+  });
 
+  const [wishlist, setWishlist] = useState ([]);
+
+
+  const apiKey = "4P5afjX98PHm5yhdSLbee6G9PVKAQGB7";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&locale=*&size=20`;
+      
+      if (filters.keyword || slug) {
+        url +=`&keyword=${filters.keyword || slug}`;
+      } 
+
+      url += `&classificationName=Music, Festival`;
+
+      if (filters.city) {
+        url +=`&city=${filters.city}`;
+      }
+
+      if (filters.country) {
+        url +=`&countryCode=${filters.country}`;
+      } 
+
+      if (filters.date) {
+        url +=`&startDateTime=${filters.date}T00:00:00Z`;
+      }
+
+      try {
+        const res = await fetch (url);
+        const data= await res.json();
+        setEvents(data._embedded?.events || []);
+      } catch (error) {
+        console.error("feil ved henting av data:", error);
+      }
+    };
+
+    fetchData();
+    }, [slug, filters]);
+
+    const handleChange = (e) => {
+      const {name, value} = e.target;
+      setFilters((prev) => ({...prev, [name]: value}));
+    };
+
+    const toggleWishlist = (id) => {
+      setWishlist((prev) =>
+      prev.includes(id)? prev.filter((x) => x !== id) : [...prev, id]
+    );
+    };
+    
   return (
-    <div>
-      <h1>{categoryName}</h1>
-    </div>
+    <>
+
+      <h1>{slug}</h1>
+
+      <div className="filters">
+        <input 
+          type="text"
+          name="keyword"
+          placeholder="Søk her"
+          value={filters.keyword}
+          onChange={handleChange}
+        />
+
+      <input 
+        type="text"
+        name="city"
+        placeholder="By"
+        value={filters.city}
+        onChange={handleChange}
+      />
+
+        <input 
+          type="text"
+          name="country"
+          placeholder="Landskode (eks. NO)"
+          value={filters.country}
+          onChange={handleChange}
+        />
+
+        <input 
+          type="date"
+          name="date"
+          placeholder="00.00.0000"
+          value={filters.date}
+          onChange={handleChange}
+        />
+      </div>
+
+      <section className="flex-section">
+        {events.map((item) => (
+        <CategoryCard
+        key={item.id}
+        item={item}
+        isSaved={wishlist.includes(item.id)}
+        onSave={() => toggleWishlist(item.id)}
+        />
+      ))}
+      </section>
+
+    </>
   );
 }
 
