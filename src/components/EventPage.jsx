d181e54686ce681cefb618455c55f051f0a11718
MirnaBuntic <mirnacb@hiof.no>
2025-05-02 22:43:25 +0200

    lagt till festivalpass i eventpage

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -5,14 +5,16 @@ import ArtistCard from "./ArtistCard";
 export default function EventPage({ attractions }) {
 
     const { slug } = useParams();
+    const [festivalPasses, setFestivalPasses] = useState([]);
     const [artists, setArtists] = useState([]);
 
     const attraction = attractions.find(attraction =>
         attraction.name.toLowerCase().replace(/\s+/g, '-') === slug
     );
 
+
     useEffect(() => {
-        const getArtists = async () => {
+        const getFestivalData = async () => {
             if (!attraction) return;
 
             const apiKey = "4P5afjX98PHm5yhdSLbee6G9PVKAQGB7";
@@ -23,15 +25,27 @@ export default function EventPage({ attractions }) {
                 const response = await fetch(url);
                 const data = await response.json();
 
-                const event = data._embedded?.events?.[0];
-                const artistList = event?._embedded?.attractions || [];
+                const events = data._embedded?.events || [];
+
+                const passes = events.map(event => ({
+                    id:event.id,
+                    name: event.name,
+                    image: event.images?.[0]?.url,
+                    date: event.dates?.start?.localDate,
+                    venue: event._embedded?.venues?.[0]?.name,
+                }));
+                setFestivalPasses(passes);
+
+                const artistList = events[0]?._embedded?.attractions || [];
                 setArtists(artistList);
+
+
              }   catch (error) {
                 console.error("Skjedde noe feil ved fetch av artister", error);
             }
         };
 
-        getArtists();
+        getFestivalData();
     }, [attraction]);
 
     if (!attraction) {
@@ -41,6 +55,28 @@ export default function EventPage({ attractions }) {
     return (
         <>
             <h2>{attraction.name}</h2>
+
+            <h3>Sjanger:</h3>
+
+            <h3>Følg oss på sosiale medier:</h3>
+
+            {festivalPasses.length > 0 && (
+                <section>
+                    <h2>Festivalpass:</h2>
+                    <ul>
+                        {festivalPasses.map(pass => (
+                            <li key={pass.id}>
+                                {pass.image && <img src={pass.image} alt={pass.name} />}
+                                <h>{pass.name}</h>
+                                <p>{pass.venue}</p>
+                                <p>{pass.date}</p>
+                                <p>Kjøp</p>
+                                <p>Legg til i ønskeliste</p>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            )}
 
             {artists.length > 0 && (
                 <section>
