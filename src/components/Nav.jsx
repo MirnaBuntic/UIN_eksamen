631148da96aa776b39f270239f6b08aa9583aeb4
Danielladominey <danielhd@hiof.no>
2025-05-12 22:52:59 +0200

    Oppdatert nav så den skriver ut  linker dynamisk og matcher slug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,17 +7,18 @@ export default function Nav() {
     return (
         <nav className="nav">
           <div className="nav_category_wrapper">
-            <Link to="/category/musikk" className="nav_category">
-              Musikk
-            </Link>
-            <Link to="/category/sport" className="nav_category">
-              Sport
-            </Link>
-            <Link to="/category/teater-show" className="nav_category">
-              Teater/Show
-            </Link>
+            {categories.map((category) =>  (
+                <Link
+                key={category.id}
+                to={`/category/${category.slug}`}
+                className="nav_category"
+                >
+                    {category.name}
+                </Link>
+            ))}
+
             <Link to="/dashboard" className="nav_logginn">
-              Logg inn
+                Logg inn
             </Link>
           </div>
         </nav>
